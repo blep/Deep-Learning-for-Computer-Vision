c5f07d1af15e804cf238e7f9dbdb3c139d5696b2
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2021-07-26 09:36:20 +0200

    Updated Chapter01 examples to use tensorflow2 compatibility layer with v1.

--- Chapter01/1_hello_tensorflow.py
+++ Chapter01/1_hello_tensorflow.py
@@ -1,4 +1,9 @@
-import tensorflow as tf
+# This was tensorflow 1.0 import:
+#import tensorflow as tf
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
 hello = tf.constant('Hello, TensorFlow!')
 session = tf.Session()
 print(session.run(hello))
--- Chapter01/3_add_tensorboard.py
+++ Chapter01/3_add_tensorboard.py
@@ -1,9 +1,13 @@
-import tensorflow as tf
+from pathlib import Path
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+LOG_DIR = Path(__file__).parent / 'training-log'
 
 x = tf.placeholder(tf.float32, name='x')
 y = tf.placeholder(tf.float32, name='y')
 z = tf.add(x, y, name='sum')
 session = tf.Session()
-summary_writer = tf.summary.FileWriter('/tmp/1', session.graph)
+summary_writer = tf.summary.FileWriter(LOG_DIR, session.graph)
 summary_writer.flush()
-
